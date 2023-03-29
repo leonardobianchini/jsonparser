eec47190ade4ef08f3a386e65066e34c8d495d9e
leonardobianchini <leonardobianchini7@gmail.com>
2023-03-28 21:58:39 -0300

    Add order class and first parser instructions

--- src/JsonParser.java
+++ src/JsonParser.java
@@ -1,27 +1,53 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 class JsonParser {
+
+  private static String formatDate(String date) {
+    String year = date.substring(0,4);
+    String month = date.substring(4,6);
+    String day = date.substring(6,8);
+    return year + "-" + month + "-" + day;
+  }
 
   public static void main(String args[]) throws FileNotFoundException, IOException {
     try (
       BufferedReader buffer = new BufferedReader(new FileReader("../inputs/data_1.txt"))
     ) {
-      StringBuilder stringBuilder = new StringBuilder();
       String line = buffer.readLine();
-      int countLines = 0;
+      ArrayList<Orders> allOrders = new ArrayList<Orders>();
+      BufferedWriter writer = new BufferedWriter(new FileWriter("parsed.json"));
   
       while (line != null) {
-          countLines++;
-          stringBuilder.append(line);
-          stringBuilder.append(System.lineSeparator());
+          Orders order = new Orders();
+
+          String idUser = line.substring(0, 10);
+          order.idUser = Integer.parseInt(idUser);
+          String name = line.substring(10, 55);
+          order.nameUser = name.trim();
+          String idOrder = line.substring(55, 65);
+          order.idOrder = Integer.parseInt(idOrder);
+          String idProd = line.substring(65, 75);
+          order.idProduct = Integer.parseInt(idProd);
+          String value = line.substring(75, 87);
+          order.valueProduct = Double.parseDouble(value);
+          String date = line.substring(87, 95);
+          order.dateOrder = formatDate(date);
+
+          allOrders.add(order);
           line = buffer.readLine();
       }
 
-      String everything = stringBuilder.toString();
-      System.out.println(everything + countLines);
+      for(Orders order: allOrders) {
+        writer.write(order.nameUser + System.lineSeparator());
+      }
+      writer.close();
+      System.out.println("Finish parser.");
     }
     catch (Exception err) {
       System.out.println("Error to run project: " + err);
---
+++ src/Orders.java
@@ -0,0 +1,8 @@
+public class Orders {
+  Integer idUser;
+  String nameUser;
+  Integer idOrder;
+  Integer idProduct;
+  Double valueProduct;
+  String dateOrder;
+}
